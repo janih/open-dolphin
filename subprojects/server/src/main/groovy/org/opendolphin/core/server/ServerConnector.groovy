5a1d5f3f069d32a637e557cdf99775cf3d5e02ad
Michael Heinrichs <michael.heinrichs@netopyr.com>
2016-03-16 16:30:33 +0100

    Fix for concurrent access to release-response

--- subprojects/server/src/main/groovy/org/opendolphin/core/server/ServerConnector.groovy
+++ subprojects/server/src/main/groovy/org/opendolphin/core/server/ServerConnector.groovy
@@ -15,20 +15,17 @@
  */
 
 package org.opendolphin.core.server
-
+import groovy.util.logging.Log
+import org.codehaus.groovy.runtime.StackTraceUtils
 import org.opendolphin.core.comm.Codec
 import org.opendolphin.core.comm.Command
 import org.opendolphin.core.comm.SignalCommand
-import org.opendolphin.core.server.action.*
-import groovy.transform.CompileStatic
-import groovy.util.logging.Log
-import org.codehaus.groovy.runtime.StackTraceUtils
+import org.opendolphin.core.server.action.DolphinServerAction
+import org.opendolphin.core.server.action.ServerAction
 import org.opendolphin.core.server.comm.ActionRegistry
 import org.opendolphin.core.server.comm.CommandHandler
 
 import java.util.logging.Level
-
-
 //CompileStatic
 @Log
 class ServerConnector {
@@ -48,8 +45,8 @@ class ServerConnector {
             for (DolphinServerAction it in dolphinServerActions) {
                 it.dolphinResponse = response       // todo: can be deleted as soon as all action refer to the SMS
             }
+            serverModelStore.currentResponse = response
         }
-        serverModelStore.currentResponse = response
 
         List<CommandHandler> actions = registry[command.id]
         if (!actions) {
