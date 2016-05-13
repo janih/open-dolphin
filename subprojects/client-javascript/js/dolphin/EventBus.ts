366ce1d17a434d2b2cfc581ff0a9e79dbab81f94
frick <sascha.frick@empros.ch>
2016-05-13 08:11:19 +0200

    Support for unregistering event handlers on EventBus, incl. tests.

--- subprojects/client-javascript/js/dolphin/EventBus.ts
+++ subprojects/client-javascript/js/dolphin/EventBus.ts
@@ -2,12 +2,30 @@ module opendolphin {
 
     export class EventBus<T> {
         private eventHandlers = [];
-        onEvent(eventHandler: (event : T) => void ) {
+
+        /**
+         * Registers eventHandler on this EventBus.
+         * The returned function can be used to unregister the handler later on.
+         *
+         * @param eventHandler
+         * @returns {function(): undefined} to unregister eventHandler
+         */
+        onEvent(eventHandler: (event : T) => void ) : ()=> void {
             this.eventHandlers.push(eventHandler);
+            return () => {
+                this.unregister(eventHandler);
+            }
         }
+
+        unregister(eventHandler) {
+          let idx = this.eventHandlers.indexOf(eventHandler);
+          if (idx > -1) {
+            this.eventHandlers.splice(idx, 1);
+          }
+        };
+
         trigger(event : T ) {
             this.eventHandlers.forEach(handle => handle(event));
         }
-
     }
 }
